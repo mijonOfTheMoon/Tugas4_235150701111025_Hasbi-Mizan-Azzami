18f180e87bb6cf09ed9501261f27259d13c9174d
mijonOfTheMoon <devopshasbi@gmail.com>
2024-05-04 11:45:50 +0700

    Screenshot Output

--- Main.java
+++ Main.java
@@ -1,15 +1,16 @@
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
+        TextBook textbook = new TextBook("Building Event-Driven Microservices", "Adam Bellemare", 2020,
+                "Information Technology");
         Novel novel = new Novel("Pulang-Pergi", "Tere Liye", 2021, "Fiction");
         Magazine magazine = new Magazine("Sindo Weekly", "Various", 2020, "News");
-        TextBook textbook = new TextBook("Mastering Cloud Computing", "Prof. Dr. Yoesoep Edhie Rachmad", 2023, "Information Technology");
 
         System.out.println("---- Book Infomation ----");
+        textbook.displayInfo();
         novel.displayInfo();
         magazine.displayInfo();
-        textbook.displayInfo();
 
         System.out.println("---- Borrowing and Returning Book ----");
         textbook.borrowBook();
